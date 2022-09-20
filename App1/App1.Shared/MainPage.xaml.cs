4eca7e9a530c739782f7bce5db3a4dee24916d7b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-09-20 15:19:35 -0300

    base work for refresh view

--- App1/App1.Shared/MainPage.xaml.cs
+++ App1/App1.Shared/MainPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Reflection;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -21,8 +14,21 @@ namespace App1;
 /// </summary>
 public sealed partial class MainPage : Page
 {
+	ObservableCollection<string> Items { get; } = new(typeof(Colors)
+		.GetProperties(BindingFlags.Public | BindingFlags.Static)
+		.Select(x => x.Name));
+
 	public MainPage()
 	{
 		this.InitializeComponent();
+		listView.ItemsSource = Items;
+	}
+
+
+	async Task DoSometing()
+	{
+		// Simulate an API call
+		await Task.Delay(TimeSpan.FromSeconds(2));
+		Items.Add($"new item {Items.Count + 1}");
 	}
 }
